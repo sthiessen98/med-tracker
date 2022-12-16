b232ca77a282944f4789ea1c5b9be86223e656d3
spencer thiessen <spencer.thiessen98@hotmail.com>
2022-12-15 19:16:26 -0800

    Removed styling based on preferred color scheme

--- src/Components/ScreenHeader.tsx
+++ src/Components/ScreenHeader.tsx
@@ -6,8 +6,8 @@ interface IScreenHeaderProps{
 
 function ScreenHeader({title}: IScreenHeaderProps){
     return(
-    <View style={{ alignItems: 'stretch', backgroundColor: Appearance.getColorScheme() === 'dark'? 'black' : 'white', flexDirection: 'row', alignContent: 'center', height: 40, marginBottom: 10, borderBottomColor: 'grey', borderBottomWidth: 2, borderStyle: 'solid' }}>
-        <Text style={{textAlign: 'center', width: '100%', fontSize:20, padding: 4, color: Appearance.getColorScheme() === 'dark' ? 'white' : 'black'}}>{title}</Text>
+    <View style={{ alignItems: 'stretch', backgroundColor: 'black', flexDirection: 'row', alignContent: 'center', height: 40, marginBottom: 10, borderBottomColor: 'grey', borderBottomWidth: 2, borderStyle: 'solid' }}>
+        <Text style={{textAlign: 'center', width: '100%', fontSize:20, padding: 4, color: 'white'}}>{title}</Text>
     </View>
     );
 }
