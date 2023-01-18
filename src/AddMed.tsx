922e09cdb15f3469452784edd58226b2b0000a4a
spencer thiessen <spencer.thiessen98@hotmail.com>
2023-01-18 09:26:22 -0800

    Added yup form validation to addMed screen

--- src/AddMed.tsx
+++ src/AddMed.tsx
@@ -6,6 +6,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import ScreenHeader from "./Components/ScreenHeader";
 import ScreenFooter from "./Components/ScreenFooter";
 import { Formik } from "formik";
+import * as yup from "yup";
 
 interface addMedProps {
     editableItem?: currMedInstance;
@@ -13,11 +14,6 @@ interface addMedProps {
 }
 
 function AddMed({onBackPress, editableItem}: addMedProps){
-
-    const [name, setName] = useState<string>(editableItem?.name ?? '');
-    const [dosage, setDosage] = useState<number>(editableItem?.dose ?? 0);
-    const [maxDosage, setMaxDosage] = useState<number>(editableItem?.maxDosage ?? 0);
-    const [interval, setInterval] = useState<number>(editableItem?.doseInterval ?? 0);
 
     const updateMedList = async (newMed: currMedInstance)=> {
         const jsonValue = await AsyncStorage.getItem('currentMeds');
@@ -29,12 +25,42 @@ function AddMed({onBackPress, editableItem}: addMedProps){
         await AsyncStorage.setItem('currentMeds', updatedMedsJson);
     }
 
+    const medValidationSchema = yup.object().shape({
+        name: yup
+            .string()
+            .min(3, ({min})=> `The drug name must be at least ${min} characters long`)
+            .required('Please enter the drug name'),
+        dosage: yup
+            .number()
+            .typeError('Please enter a whole number')
+            .integer('Please enter a whole number')
+            .moreThan(0, 'Please enter a number greater than 0')
+            .required('Please enter the drug dosage'),
+        maxDosage: yup
+            .number()
+            .typeError('Please enter a whole number')
+            .nullable()
+            .integer('Please enter a whole number')
+            .min(yup.ref('dosage')),
+        doseInterval: yup
+            .number()
+            .typeError('Please enter a whole number')
+            .nullable()
+            .integer('Please enter a whole number')
+            .moreThan(0, 'Please enter a number greater than 0'),
+    });
 
     return(
         <View style={{backgroundColor: '#ecf4f4',height: '100%', width: '100%', flexDirection: 'column', justifyContent: 'space-between'}}>
             <ScreenHeader title={editableItem ? 'Edit Med' : 'Add Med'}/>
-                <Formik 
-                initialValues={{name: name, dosage: dosage.toString(), maxDosage: maxDosage.toString(), interval: interval.toString()}}
+                <Formik
+                initialValues={{
+                    name: editableItem?.name ?? '', 
+                    dosage: editableItem?.dose?.toString() ?? '0', 
+                    maxDosage: editableItem?.maxDosage?.toString() ?? '', 
+                    interval: editableItem?.doseInterval?.toString() ?? ''
+                }}
+                validationSchema={medValidationSchema}
                 onSubmit={async (values)=> {
                     if(values.name !== null && parseInt(values.dosage) > 0){
                         const newMed: currMedInstance = {
@@ -48,15 +74,18 @@ function AddMed({onBackPress, editableItem}: addMedProps){
                             onBackPress();
                         }
                 }}>
-                    {({ handleChange, handleSubmit, values }) => (
-                    <View style={{flexDirection:'column', justifyContent: 'space-between'}}>
+                    {({ handleChange, handleSubmit, values, errors }) => (
+                    <View style={{flexDirection:'column', justifyContent: 'space-between', height: '80%'}}>
                         <View style={{justifyContent: 'flex-start'}}>
                             <View style={{flexDirection: 'column'}}>
-                                <Text style={Styles.textStyle}>Name:</Text>
+                                <Text style={Styles.textStyle}>Drug Name:</Text>
                                 <TextInput style={Styles.textInput}
                                 onChangeText={handleChange('name')}
                                 value={values.name}
                                 />
+                            {errors.name && (
+                                <Text style={Styles.errorText}>{errors.name}</Text>
+                            )}
                             </View>
                             <View style={Styles.viewStyle}>
                                 <Text style={Styles.textStyle}>Dosage:</Text>
@@ -64,6 +93,9 @@ function AddMed({onBackPress, editableItem}: addMedProps){
                                 onChangeText={handleChange('dosage')}
                                 value={values.dosage}
                                 />
+                            {errors.dosage && (
+                                <Text style={Styles.errorText}>{errors.dosage}</Text>
+                            )}
                             </View>
                             <View style={Styles.viewStyle}>
                                 <Text style={Styles.textStyle}>Max Dosage:</Text>
@@ -72,15 +104,21 @@ function AddMed({onBackPress, editableItem}: addMedProps){
                                 value={values.maxDosage}
                                 />
                             </View>
+                            {errors.maxDosage && (
+                                <Text style={Styles.errorText}>{errors.maxDosage}</Text>
+                            )}
                             <View style={Styles.viewStyle}>
                                 <Text style={Styles.textStyle}>Time Between Doses(Hours):</Text>
                                 <TextInput style={Styles.textInput}
                                 onChangeText={handleChange('interval')}
                                 value={values.interval}
                                 />
+                            {errors.interval && (
+                                <Text style={Styles.errorText}>{errors.interval}</Text>
+                            )}
                             </View>
                         </View>
-                        <View style={{justifyContent: 'flex-end'}}>
+                        <View style={{justifyContent: 'flex-end', backgroundColor: 'red', marginLeft: 20, marginRight: 20, elevation: 15}}>
                             <Button color={'#007560'} onPress={handleSubmit} title="Submit" />
                         </View>
                      </View>
@@ -96,8 +134,10 @@ function AddMed({onBackPress, editableItem}: addMedProps){
 
 const Styles = {
     textStyle: {
+        marginLeft: 5,
         color: 'black',
-         fontSize: 12
+         fontSize: 12,
+         paddingTop: 5
         },
     viewStyle: {
         flexDirection: 'column' as const,
@@ -107,7 +147,15 @@ const Styles = {
         backgroundColor: 'grey',
         paddingLeft: 5, 
         marginLeft: 5, 
-        marginRight: 5
+        marginRight: 5,
+        border: 'black',
+        borderWidth: 1,
+    },
+    errorText: {
+        color: 'red',
+        paddingLeft: 5,
+        marginLeft: 5,
+        marginRight: 5,
     }
 }
 
