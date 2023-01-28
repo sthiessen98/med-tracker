54a0922dbd9839b3e926a157d7b4d2ce6cc145fb
spencer thiessen <spencer.thiessen98@hotmail.com>
2023-01-28 12:01:27 -0800

    Properly calculate last taken time and display only when we've hit our max dosage

--- src/MedListItem.tsx
+++ src/MedListItem.tsx
@@ -1,7 +1,8 @@
 import React from "react";
-import { Alert, Button, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { Alert, Text, TouchableOpacity, View } from "react-native";
 import { currMedInstance, medLogInstance } from "./App";
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import EntypoIcon from 'react-native-vector-icons/Entypo';
 import uuid from 'react-native-uuid';
 
 interface MedListItemProps {
@@ -14,19 +15,31 @@ interface MedListItemProps {
 
 function MedListItem({med, logs, refetch, editMode, onEditPress}: MedListItemProps){
 
+    const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"];
+
     //Calculate if we've reached max dosage in allotted time frame
     const currentTime = new Date();
     const cutOffTime = new Date(currentTime.setHours(currentTime.getHours()- (med?.doseInterval ?? 0)));
     const doseTakenRecently: number = logs.filter((log) => new Date(log.time).toISOString() > cutOffTime.toISOString()).reduce((partialSum, a)=> partialSum + a.dose, 0);
     const takenRecently = Boolean(doseTakenRecently >= (med?.maxDosage ?? 0) && med?.maxDosage && med?.doseInterval);
 
+    //Calculate time to when we can take next dose
+    let minutesToNextDose: number;
+    const oldestDose =  logs.filter((log) => new Date(log.time).toISOString() > cutOffTime.toISOString()).sort((a,b)=> a.time > b.time ? 1 : -1)?.[0] ?? undefined;
+
+    if(oldestDose !== undefined){
+        const oldestDoseDateDiff = new Date(oldestDose.time).getTime()- cutOffTime.getTime();
+        minutesToNextDose = Math.floor(((oldestDoseDateDiff/1000)/60)); //miliseconds -> Minutes
+    }else{
+        minutesToNextDose = 0;
+    }
+
     //Do we display Last Taken info
-    const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"];
     const lastTaken: medLogInstance | undefined = logs.length ? logs?.sort((a,b)=> a.time > b.time ? 1 : -1)?.[0] : undefined;
     let lastTakenDisplay: string;
     if(lastTaken?.time !== undefined){
         const lastTakenDate= new Date(lastTaken.time);
-        lastTakenDisplay = `Taken: ${lastTakenDate.getHours() < 10 ? 0 : ''}${lastTakenDate.getHours()}:${lastTakenDate.getMinutes() < 10 ? 0 : ''}${lastTakenDate.getMinutes()} ${monthNames[lastTakenDate.getMonth()]} ${lastTakenDate.getDate()}`;
+        lastTakenDisplay = `Taken: ${monthNames[lastTakenDate.getMonth()]} ${lastTakenDate.getDate()} ${lastTakenDate.getHours() < 10 ? 0 : ''}${lastTakenDate.getHours()}:${lastTakenDate.getMinutes() < 10 ? 0 : ''}${lastTakenDate.getMinutes()}`;
     }else{
         lastTakenDisplay = '  ';
     }
@@ -97,18 +110,28 @@ function MedListItem({med, logs, refetch, editMode, onEditPress}: MedListItemPro
             </View>
             <View className='flex basis-2/5 w-full mx-1'>
                 {editMode && (
-                    <View className='flex-row'>
-                        <Button title="Edit" onPress={()=> onEditPress(med)}/>
-                        <Button title="Delete" onPress={()=> showDeleteConfirmation(med)}/>
+                    <View className='flex-row justify-end space-x-2'>
+                        <TouchableOpacity className='items-center flex-col w-[55px] bg-orange-300 border-2 rounded-lg p-1' style={{elevation: 6}} onPress={()=> onEditPress(med)}>
+                            <EntypoIcon name='edit' size={20} color="black"/> 
+                            <Text className='text-black text-sm'>Edit</Text>    
+                        </TouchableOpacity>
+                        <TouchableOpacity className='items-center flex-col w-[55px] bg-red-500 border-2 rounded-lg p-1' style={{elevation: 6}} onPress={()=> showDeleteConfirmation(med)}>
+                            <EntypoIcon name='trash' size={20} color="black"/> 
+                            <Text className='text-black text-sm'>Delete</Text>    
+                        </TouchableOpacity>
                     </View>
                 )}
-                <View className='flex-col items-end w-full h-full mr-1'>
+                {!editMode && (
+                    <View className='flex-col items-end w-full h-full mr-1'>
                     {!!lastTaken?.time && (
                         <Text className='basis-1/2 text-black italic'>{lastTakenDisplay}</Text>
-                    )}
-                     <Text className='basis-1/2 text-red-500 text-x italic font-bold'>Do not take for 13:32</Text>
-
-                </View>
+                     )}
+                     {minutesToNextDose !== 0 && takenRecently && (
+                        <Text className='basis-1/2 text-red-500 text-x italic font-bold'>Do not take for {Math.floor(minutesToNextDose/60)}:{minutesToNextDose%60}</Text>
+                     )}
+ 
+                 </View>
+                )}
             </View>
             </TouchableOpacity>
 
